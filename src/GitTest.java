20876eff4bf11e7807ca971fd5b8bb279e7f4414
lsz123-ai <85754543+lsz123-ai@users.noreply.github.com>
2022-03-31 09:25:28 +0800

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello gitee");
         System.out.println("hello pull gitee");
         System.out.println("hello push github");
+        System.out.println("hello pull github");
     }
 }
